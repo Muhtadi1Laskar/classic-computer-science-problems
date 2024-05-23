eeff5176cfd093f282ca3410e2b67ebcf197a60a
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-05-24 02:12:41 +0600

    Added fib cache to chapter 1

--- Chapter-1/fibonacci.py
+++ Chapter-1/fibonacci.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 def fib(n: int) -> int:
     if n <= 2:
         return 1
@@ -11,6 +13,12 @@ def fib_memo(n: int, memo: dict[int, int] = {}) -> int:
     memo[n] = fib_memo(n-1, memo) + fib_memo(n-2, memo)
     return memo[n]
 
+@lru_cache(maxsize=None)
+def fib_memo_cache(n: int) -> int:
+    if n <= 2:
+        return 1
+    return fib_memo_cache(n-1) + fib_memo_cache(n-2)
 
 print(fib(10))
 print(fib_memo(100))
+print(fib_memo_cache(100))
