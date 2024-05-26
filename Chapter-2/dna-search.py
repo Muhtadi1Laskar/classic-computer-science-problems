4c4c1caddba275e119e1eebbaa166d25b9ef485b
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-05-26 23:47:08 +0600

    Added Linear Search in dna-search

--- Chapter-2/dna-search.py
+++ Chapter-2/dna-search.py
@@ -15,3 +15,15 @@ def string_to_gene(s: str) -> Gene:
         codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i + 1]], Nucleotide[s[i + 2]])
         gene.append(codon)
     return gene
+
+def linear_contains(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+    return False
+
+
+acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
+gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
+print(linear_contains(my_gene, acg))  # True
+print(linear_contains(my_gene, gat))  # False
